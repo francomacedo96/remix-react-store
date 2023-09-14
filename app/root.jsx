b8ad089f3f4fce0973d3e2794a9ab95d5b16b057
francomacedo96 <francomacedo@outlook.com>
2023-09-14 15:44:49 -0300

    meta

--- app/root.jsx
+++ app/root.jsx
@@ -1,33 +1,37 @@
-import { cssBundleHref } from "@remix-run/css-bundle";
-
 import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+    Meta
+} from "@remix-run/react"
 
-export const links = () => [
-  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
-];
+export function meta() {
+    return (
+        [
+            {
+                charset: "utf-8",
+                title: "GuitarLA-Remix",
+                viewport: "width=device-width,initial-scale=1"
+            }
+        ]
+    )
+}
 
 export default function App() {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
-    </html>
-  );
+    return (
+        <Document>
+            <h1>hola mundo</h1>
+        </Document>
+    )
+}
+
+function Document({ children }) {
+    return (
+        <html>
+            <head>
+                <Meta />
+            </head>
+
+            <body>
+                {children}
+            </body>
+        </html>
+    )
 }
